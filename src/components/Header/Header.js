05e4c6b467491fd27bd44a705a984fb87a53ee15
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-04-28 11:37:17 +0200

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import logo from "@/assets/edc-logo.svg";
+import Image from "next/image";
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="wrapper">
+      <Link href="/" aria-label="EDC">
+        <Image
+          className={styles.logo}
+          src={logo.src}
+          width={90}
+          height={90}
+          alt="EDC"
+          priority
+        />
+      </Link>
+    </header>
+  );
+}
