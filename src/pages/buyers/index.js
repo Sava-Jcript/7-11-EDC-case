b1aed1a6c5ecd0941d5edd61facccd334c2d8a71
Ali Merkouk <63125615+Sava-Jcript@users.noreply.github.com>
2023-05-03 09:48:40 +0200

    Branch 4 Brunch

--- src/pages/buyers/index.js
+++ src/pages/buyers/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import styles from "../../pages/buyers/Buyers.module.css";
-import { useEffect, useState } from "react";
+
 import { BuyerCard } from "@/components/Header/BuyerCard";
 
 
